011472fe2214fd6bfd245eaa9acbde863e231141
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2021-11-14 12:01:34 -0700

    Added zeroshot example

---
+++ examples/zeroshot/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/TannerKvarfordt/hfapigo"
+)
+
+func main() {
+	inputs := []string{
+		"Hi, I recently bought a device from your company but it is not working as advertised and I would like to get reimbursed!",
+		"Hi, I am having a difficult time using your product. Do you have a manual or perhaps a list of frequently asked questions to help get me going?",
+	}
+	candidateLabels := []string{"refund", "legal", "faq"}
+	endpoint := hfapigo.APIBaseURL + hfapigo.RecommendedZeroShotModel
+
+	fmt.Printf("Inputs: %s\n", inputs[0])
+	fmt.Printf("CandidateLabels: %v\n", candidateLabels)
+	fmt.Printf("Model: %s\n", hfapigo.RecommendedZeroShotModel)
+	fmt.Printf("Sending request")
+
+	type ChanRv struct {
+		resps []*hfapigo.ZeroShotResponse
+		err   error
+	}
+	ch := make(chan ChanRv)
+
+	go func() {
+		zresps, err := hfapigo.SendZeroShotRequest(endpoint, &hfapigo.ZeroShotRequest{
+			Inputs: inputs,
+			Parameters: hfapigo.ZeroShotParameters{
+				CandidateLabels: candidateLabels,
+			},
+			Options: *hfapigo.NewOptions().SetWaitForModel(true),
+		})
+
+		ch <- ChanRv{zresps, err}
+	}()
+
+	for {
+		select {
+		case chrv := <-ch:
+			if chrv.err != nil {
+				fmt.Println(chrv.err)
+				return
+			}
+
+			fmt.Println()
+			for _, resp := range chrv.resps {
+				fmt.Println("\nSequence:", resp.Sequence)
+				for i, label := range resp.Labels {
+					fmt.Printf("%s: %f\n", label, resp.Scores[i])
+				}
+			}
+			return
+
+		default:
+			fmt.Print(".")
+			time.Sleep(time.Millisecond * 50)
+		}
+	}
+}
