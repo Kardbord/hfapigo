fb5dae88e81bdf5a2c90cb8000f096be453e3761
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2021-11-17 20:16:44 -0700

    Added support for Question Answering request

---
+++ question_answering.go
@@ -0,0 +1,62 @@
+package hfapigo
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+const RecommendedQuestionAnsweringModel = "deepset/roberta-base-squad2"
+
+// Request structure for question answering model
+type QuestionAnsweringRequest struct {
+	// (Required)
+	Inputs  QuestionAnsweringInputs `json:"inputs,omitempty"`
+	Options Options                 `json:"options,omitempty"`
+}
+
+type QuestionAnsweringInputs struct {
+	// (Required) The question as a string that has an answer within Context.
+	Question string `json:"question,omitempty"`
+
+	// (Required) A string that contains the answer to the question
+	Context string `json:"context,omitempty"`
+}
+
+// Response structure for question answering model
+type QuestionAnsweringResponse struct {
+	// A string that’s the answer within the Context text.
+	Answer string `json:"answer,omitempty"`
+
+	// A float that represents how likely that the answer is correct.
+	Score float64 `json:"score,omitempty"`
+
+	// The string index of the start of the answer within Context.
+	Start int `json:"start,omitempty"`
+
+	// The string index of the stop of the answer within Context.
+	End int `json:"end,omitempty"`
+}
+
+func SendQuestionAnsweringRequest(model string, request *QuestionAnsweringRequest) (*QuestionAnsweringResponse, error) {
+	if request == nil {
+		return nil, errors.New("nil QuestionAnsweringRequest")
+	}
+
+	jsonBuf, err := json.Marshal(request)
+	if err != nil {
+		return nil, err
+	}
+
+	respBody, err := MakeHFAPIRequest(jsonBuf, model)
+	if err != nil {
+		return nil, err
+	}
+
+	qaResp := QuestionAnsweringResponse{}
+	err = json.Unmarshal(respBody, &qaResp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &qaResp, nil
+}
---
+++ question_answering_test.go
@@ -0,0 +1,115 @@
+package hfapigo_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/TannerKvarfordt/hfapigo"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestMarshalUnMarshalQARequest(t *testing.T) {
+	// No options
+	{
+		qaExpected := hfapigo.QuestionAnsweringRequest{
+			Inputs: hfapigo.QuestionAnsweringInputs{
+				Question: "What's my name?",
+				Context:  "My name is Clara and I live in Berkeley.",
+			},
+		}
+
+		jsonBuf, err := json.Marshal(qaExpected)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		qaActual := hfapigo.QuestionAnsweringRequest{}
+		err = json.Unmarshal(jsonBuf, &qaActual)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !cmp.Equal(qaExpected, qaActual) {
+			t.Fatalf("Expected %v, got %v", qaExpected, qaActual)
+		}
+	}
+
+	// Options
+	{
+		qaExpected := hfapigo.QuestionAnsweringRequest{
+			Inputs: hfapigo.QuestionAnsweringInputs{
+				Question: "What's my name?",
+				Context:  "My name is Clara and I live in Berkeley.",
+			},
+			Options: *hfapigo.NewOptions().SetWaitForModel(true),
+		}
+
+		jsonBuf, err := json.Marshal(qaExpected)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		qaActual := hfapigo.QuestionAnsweringRequest{}
+		err = json.Unmarshal(jsonBuf, &qaActual)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !cmp.Equal(qaExpected, qaActual) {
+			t.Fatalf("Expected %v, got %v", qaExpected, qaActual)
+		}
+	}
+}
+
+func TestQARequest(t *testing.T) {
+	// Basic request
+	{
+		qaResp, err := hfapigo.SendQuestionAnsweringRequest(hfapigo.RecommendedQuestionAnsweringModel, &hfapigo.QuestionAnsweringRequest{
+			Inputs: hfapigo.QuestionAnsweringInputs{
+				Question: "What's my name?",
+				Context:  "My name is Clara and I live in Berkeley.",
+			},
+			Options: *hfapigo.NewOptions().SetWaitForModel(true),
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		if qaResp == nil {
+			t.Fatalf("Expected non-nil response")
+		}
+
+		const (
+			expectedAnswer = "Clara"
+			expectedStart  = 11
+			expectedEnd    = 16
+		)
+		if qaResp.Answer != expectedAnswer {
+			t.Fatalf("Expected %s, got %s", expectedAnswer, qaResp.Answer)
+		}
+		if qaResp.Score == 0.0 {
+			t.Fatal("Expected non-zero confidence")
+		}
+		if qaResp.Start != expectedStart {
+			t.Fatalf("Expected %d, got %d", expectedStart, qaResp.Start)
+		}
+		if qaResp.End != expectedEnd {
+			t.Fatalf("Expected %d, got %d", expectedEnd, qaResp.End)
+		}
+	}
+
+	// Invalid request
+	{
+		qaResp, err := hfapigo.SendQuestionAnsweringRequest(hfapigo.RecommendedQuestionAnsweringModel, &hfapigo.QuestionAnsweringRequest{
+			Inputs: hfapigo.QuestionAnsweringInputs{
+				Question: "What's my name?",
+			},
+			Options: *hfapigo.NewOptions().SetWaitForModel(true),
+		})
+		if err == nil {
+			t.Fatal("Expected error - invalid request")
+		}
+		if qaResp != nil {
+			t.Fatalf("Expected nil response - invalid request")
+		}
+	}
+}
