36c091d2204f771bf2c4acbd9b3db85bf5fda795
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2023-04-08 16:21:05 -0600

    Got segments drawing in image_segmentation demo

--- examples/image_segmentation/main.go
+++ examples/image_segmentation/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"fmt"
 	"image"
+	"image/color"
 	"image/draw"
 	"image/png"
 	"math/rand"
@@ -30,45 +31,33 @@ const (
 )
 
 func main() {
-	fmt.Printf("Opening image: %s\n", inputImage)
-	srcImg, err := OpenImg(inputImage)
-	if err != nil {
-		fmt.Println("Problem opening image:", err)
-		return
-	}
-
-	fmt.Printf("Requesting segmentation of image: %s\n", inputImage)
-	resps, err := hfapigo.SendImageSegmentationRequest(hfapigo.RecommendedImageSegmentationModel, inputImage)
+	resps, err := SendRequest()
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
-	for _, r := range resps {
-		reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(r.Mask))
-		mask, _, err := image.Decode(reader)
-		if err != nil {
-			fmt.Println("Problem decoding mask:", err)
-			return
-		}
-		draw.DrawMask(srcImg, srcImg.Bounds(), mask, image.Point{}, mask, image.Point{}, draw.Src)
-	}
-
-	outf, err := os.Create(outputImage)
+	srcImg, err := OpenImg(inputImage)
 	if err != nil {
-		fmt.Printf("Error creating %s -> %s\n", outputImage, err)
-	}
-	defer outf.Close()
-
-	err = png.Encode(outf, srcImg)
-	if err != nil {
-		fmt.Println("Problem encoding output file:", err)
+		fmt.Println("Error:", err)
 		return
 	}
-	fmt.Println("Output image written to", outf.Name())
+
+	err = DrawMasks(srcImg, resps)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	err = WriteOutputFile(srcImg, outputImage)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 }
 
 func OpenImg(imgFile string) (draw.Image, error) {
+	fmt.Printf("Opening image: %s\n", imgFile)
 	f, err := os.Open(inputImage)
 	if err != nil {
 		return nil, err
@@ -80,4 +69,59 @@ func OpenImg(imgFile string) (draw.Image, error) {
 		return nil, fmt.Errorf("%T is not a drawable image type", img)
 	}
 	return dimg, err
+}
+
+func SendRequest() ([]*hfapigo.ImageSegmentationResponse, error) {
+	fmt.Printf("Requesting segmentation of image: %s\n", inputImage)
+	return hfapigo.SendImageSegmentationRequest(hfapigo.RecommendedImageSegmentationModel, inputImage)
+}
+
+// See https://go.dev/blog/image-draw
+func DrawMasks(srcImg draw.Image, resps []*hfapigo.ImageSegmentationResponse) error {
+	for segmentN, r := range resps {
+		reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(r.Mask))
+		mask, _, err := image.Decode(reader)
+		if err != nil {
+			return err
+		}
+		WriteOutputFile(mask, fmt.Sprintf("mask%d.png", segmentN))
+		fmt.Printf("Label: %s, Score: %f\n", r.Label, r.Score)
+
+		segment := image.NewRGBA(mask.Bounds())
+		segmentColor := color.RGBA{
+			R: uint8(rand.Intn(266)),
+			G: uint8(rand.Intn(266)),
+			B: uint8(rand.Intn(266)),
+			A: 210,
+		}
+
+		// There's probably a better way to do this, but image
+		// processing is not my forte.
+		for x := 0; x < segment.Bounds().Dx(); x++ {
+			for y := 0; y < segment.Bounds().Dy(); y++ {
+				mr, mg, mb, ma := mask.At(x, y).RGBA()
+				if mr+mg+mb == ma*3 {
+					segment.SetRGBA(x, y, segmentColor)
+				}
+			}
+		}
+		WriteOutputFile(segment, fmt.Sprintf("segment%d.png", segmentN))
+		draw.DrawMask(srcImg, srcImg.Bounds(), segment, image.Point{}, mask, image.Point{}, draw.Over)
+	}
+	return nil
+}
+
+func WriteOutputFile(img image.Image, filename string) error {
+	outf, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer outf.Close()
+
+	err = png.Encode(outf, img)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Output image written to", outf.Name())
+	return nil
 }
