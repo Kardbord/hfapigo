88b9df0fb920ba357af9143cc7e4e39b66f2184a
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2023-04-08 10:43:40 -0600

    Added image_classification example

---
+++ examples/image_classification/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/TannerKvarfordt/hfapigo"
+)
+
+
+const HuggingFaceTokenEnv = "HUGGING_FACE_TOKEN"
+
+func init() {
+	key := os.Getenv(HuggingFaceTokenEnv)
+	if key != "" {
+		hfapigo.SetAPIKey(key)
+	}
+}
+
+const inputImg = "./test-image.png"
+
+func main() {
+	fmt.Printf("Requesting classification of image: %s\n", inputImg)
+	resps, err := hfapigo.SendImageClassificationRequest(hfapigo.RecommendedImageClassificationModel, inputImg)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err);
+		return
+	}
+
+	for _, r := range resps {
+		fmt.Printf("%s: %f\n", r.Label, r.Score)
+	}
+}
