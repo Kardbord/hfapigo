c87e09dcdc5e2508183b62dbf4f0409f35377ccf
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2023-04-08 11:09:41 -0600

    Added image_segmentation_test.go

---
+++ image_segmentation_test.go
@@ -0,0 +1,31 @@
+package hfapigo_test
+
+import (
+	"testing"
+
+	"github.com/TannerKvarfordt/hfapigo"
+)
+
+func TestImageSegmentationRequest(t *testing.T) {
+	resps, err := hfapigo.SendImageSegmentationRequest(hfapigo.RecommendedImageSegmentationModel, TestFilesDir+"/test-image.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(resps) == 0 {
+		t.Fatal("expected non-empty response")
+	}
+	for _, resp := range resps {
+		if resp == nil {
+			t.Fatal("nil response received")
+		}
+		if resp.Score == 0.0 {
+			t.Fatal("expected non-zero score")
+		}
+		if resp.Label == "" {
+			t.Fatal("expected non-empty label")
+		}
+		if resp.Mask == "" {
+			t.Fatal("expected non-empty mask")
+		}
+	}
+}
