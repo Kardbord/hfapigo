f264b10b283ea6165bc159c761402ac3b544b97e
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2024-02-10 21:54:58 -0700

    Added build script

---
+++ tools/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+pushd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null
+
+echo "Formatting code..."
+go fmt ./...
+echo "Building $(basename "$(pwd)")"
+go build ./...
+echo "Vetting..."
+go vet ./...
+echo "Running tests..."
+go test ./...
+echo "Done."
