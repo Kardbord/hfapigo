0a283f7ec193ef94fc7c7a8facca287ad3833fd2
Tanner Kvarfordt <Tanner.Kvarfordt@gmail.com>
2026-02-06 14:19:37 -0800

    Separated api package files by concern

--- api/chat.go
+++ api/chat.go
@@ -1,10 +1,5 @@
 package api
 
-import "github.com/Kardbord/hfapigo/v4/internal/request"
-
-// RequestOption represents a functional option that configures client requests.
-type RequestOption = request.RequestOption
-
 // ChatRequest represents a completion request for the chat API.
 type ChatRequest struct {
 	Inputs string `json:"inputs"`
---
+++ api/options.go
@@ -0,0 +1,6 @@
+package api
+
+import "github.com/Kardbord/hfapigo/v4/internal/request"
+
+// RequestOption represents a functional option that configures client requests.
+type RequestOption = request.RequestOption
