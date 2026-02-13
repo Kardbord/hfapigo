b877871952ed201c41009685006a82042d2b1b2a
Tanner Kvarfordt <Tanner.Kvarfordt@gmail.com>
2026-02-12 16:44:24 -0800

    Updated build.sh to run additional checks

--- tools/build.sh
+++ tools/build.sh
@@ -4,10 +4,23 @@ set -e
 pushd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null
 
 echo "Formatting code..."
-go fmt ./...
+gofmt -s -w .
+
+echo "Tidying module files..."
+go mod tidy
+
 echo "Vetting..."
 go vet ./...
+
 echo "Building ..."
 go build ./...
+
 echo "Running tests..."
 go test ./...
+
+echo "Checking for race conditions..."
+go test -race ./...
+
+echo "Reporting code coverage..."
+go test -cover ./...
+
