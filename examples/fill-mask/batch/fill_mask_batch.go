51856f28028067cb021b1cafbb07932eee31bb41
Tanner Kvarfordt <tanner.kvarfordt@proton.me>
2026-08-06 22:16:12 -0700

    Add fill-mask examples

---
+++ examples/fill-mask/batch/fill_mask_batch.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Kardbord/hfgo/v4"
+)
+
+func main() {
+	token := os.Getenv("HF_TOKEN")
+	if token == "" {
+		log.Fatal("HF_TOKEN environment variable is not set")
+	}
+
+	// Create a new client with your API token and desired model
+	client := hfgo.NewClient(
+		hfgo.WithToken(token),
+		hfgo.WithModel("google-bert/bert-base-uncased"),
+	)
+
+	inputs := []string{
+		"I [MASK] my dog everyday.",
+		"She is a [MASK] programmer.",
+		"The meeting was [MASK] due to the storm.",
+	}
+
+	fmt.Println("Filling the mask in inputs:")
+	PrintJSON(inputs)
+	fmt.Println("...")
+
+	// Make the batched fill mask request
+	predictions, err := client.FillMask().FillMaskBatch(
+		hfgo.FillMaskBatchRequest{
+			Inputs: inputs,
+		},
+	)
+	if err != nil {
+		log.Fatalf("error running batched fill mask: %v\n", err)
+	}
+
+	fmt.Println("Results:")
+	PrintJSON(predictions)
+}
+
+func Ptr[T any](v T) *T {
+	return &v
+}
+
+func PrintJSON[T any](v T) {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		log.Fatalf("error printing JSON: %v\n", err)
+	}
+
+	fmt.Println(string(b))
+}
---
+++ examples/fill-mask/params/fill_mask_params.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Kardbord/hfgo/v4"
+)
+
+func main() {
+	token := os.Getenv("HF_TOKEN")
+	if token == "" {
+		log.Fatal("HF_TOKEN environment variable is not set")
+	}
+
+	// Create a new client with your API token and desired model
+	client := hfgo.NewClient(
+		hfgo.WithToken(token),
+		hfgo.WithModel("google-bert/bert-base-uncased"),
+	)
+
+	input := "The quick brown fox jumps over the [MASK] dog."
+
+	targets := []string{
+		"lazy",
+		"smart",
+		"fast",
+	}
+
+	fmt.Println("Filling the mask in input:")
+	PrintJSON(input)
+	fmt.Println("Restricting predictions to targets:")
+	PrintJSON(targets)
+	fmt.Println("...")
+
+	// Make the fill mask request with parameters
+	predictions, err := client.FillMask().FillMask(
+		hfgo.FillMaskRequest{
+			Input: input,
+			Parameters: &hfgo.FillMaskParameters{
+				TopK:    Ptr(5),
+				Targets: targets,
+			},
+		},
+	)
+	if err != nil {
+		log.Fatalf("error running fill mask: %v\n", err)
+	}
+
+	fmt.Println("Results:")
+	PrintJSON(predictions)
+}
+
+func Ptr[T any](v T) *T {
+	return &v
+}
+
+func PrintJSON[T any](v T) {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		log.Fatalf("error printing JSON: %v\n", err)
+	}
+
+	fmt.Println(string(b))
+}
